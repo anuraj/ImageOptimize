e9c9530e3c43857304f90532cabe775dc433d37a
anuraj <anuraj.p@msn.com>
2024-01-01 21:51:10 +0530

    .NET Framework upgrade. And NuGet publishing changes.

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
-# ImageOptimize
+# Image Optimize
 
-A dotnet CLI tool for image optimization.
+This is a dotnet CLI tool which helps you to reduce the size of the Images.
 
-## How to use ImageOptimize?
+## Get started
+Install .NET 6 or newer and run this command:
 
-ImageOptimize is a dotnet CLI tool which will help you to modify image quality while publishing the image. You can run this tool as standalone as well. This tool is using [ImageProcessorCore](https://github.com/JimBobSquarePants/ImageProcessor) for manipulating the images. To use ImageOptimize first you need to reference ImageOptimize in your project.json in the tools section.
-```
-"tools": {
-  "BundlerMinifier.Core": "2.0.238",
-  "Microsoft.AspNetCore.Razor.Tools": "1.0.0-preview2-final",
-  "Microsoft.AspNetCore.Server.IISIntegration.Tools": "1.0.0-preview2-final",
-  "Microsoft.EntityFrameworkCore.Tools": "1.0.0-preview2-final",
-  "ImageOptimize" : "1.0.0"
-}
-```
+`dotnet tool install --global dotnet-image-optimize`
 
-And you need to run the `dotnet restore` command, which will download the ImageOptimize to your system. Once `dotnet restore` successfully completed. You can run the ImageOptimize tool using `dotnet imgopt` command, by default the tool will compress all the JPG, PNG, and GIF images inside the current directory and sub directories. Default image quality will be 10. You can customize these settings using `ImageOptimize.json` file. Here is the structure of the file.
+Run the tool with `dotnet-image-optimize` command.
+
+## Usage
 
 ```
-{
-    "sourceDirectory": "C:\\SampleWebApp",
-    "imageTypes": "*.jpg;*.png;*.gif",
-    "quality" : "100"
-}
-```
-In the configuration file, sourceDirectory should be absolute path, imageTypes should be string, seperated by semi-colon, and quality is number.
+Description:
+  Image Optimize - A simple command line tool to optimize images in a directory.
 
-[![Build status](https://ci.appveyor.com/api/projects/status/xqbtp576j3li8oqf?svg=true)](https://ci.appveyor.com/project/anuraj/imageoptimize)
+Usage:
+  dotnet-image-optimize <source> [options]
+
+Arguments:
+  <source>  Source images directory [default: C:\Windows]
+
+Options:
+  -o, --output <output>    Output directory
+  -q, --quality <quality>  Image quality (1-100) [default: 75]
+  --version                Show version information
+  -?, -h, --help           Show help and usage information
+```
---
+++ Src/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.CommandLine;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+
+var outputOption = new Option<string>(
+    aliases: new[] { "-o", "--output" },
+    description: "Output directory")
+{
+    IsRequired = false
+};
+
+var qualityOption = new Option<int>(
+    aliases: new[] { "-q", "--quality" },
+    description: "Image quality (1-100)")
+{
+    IsRequired = false
+};
+
+qualityOption.SetDefaultValue(75);
+
+var rootCommand = new RootCommand
+{
+    outputOption,
+    qualityOption
+};
+
+rootCommand.Description = "Image Optimize - A simple command line tool to optimize images in a directory.";
+
+rootCommand.Name = "dotnet-image-optimize";
+
+var sourceArgument = new Argument<string>("source")
+{
+    Arity = ArgumentArity.ExactlyOne,
+    Description = "Source images directory"
+};
+
+var supportedFormats = new[] { ".png", ".jpg", ".jpeg", ".gif" };
+
+sourceArgument.SetDefaultValue(Directory.GetCurrentDirectory());
+rootCommand.Add(sourceArgument);
+
+rootCommand.SetHandler((source, output, quality) =>
+{
+    try
+    {
+        if (!Directory.Exists(source))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Source directory {source} does not exist.");
+            Console.ResetColor();
+        }
+
+        if (string.IsNullOrEmpty(output))
+        {
+            output = source;
+        }
+        else
+        {
+            if (!Directory.Exists(output))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Output directory does not exist. Creating it now.");
+                Console.ResetColor();
+            }
+        }
+
+        var files = Directory.GetFiles(source, "*.*", SearchOption.AllDirectories)
+            .Where(file => supportedFormats.Contains(Path.GetExtension(file).ToLowerInvariant()));
+
+        if (!files.Any())
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"No images found in the source directory {source}");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Total images found : {files.Count()}, starting optimization with quality {quality}.");
+            Console.ResetColor();
+            long totalImageSizeSaved = 0;
+            foreach (var file in files)
+            {
+                long originalSize = new FileInfo(file).Length;
+                using var image = Image.Load(file);
+                var encoder = new JpegEncoder
+                {
+                    Quality = quality
+                };
+                image.Save(file, encoder);
+                long compressedSize = new FileInfo(file).Length;
+                long sizeSaved = originalSize - compressedSize;
+                totalImageSizeSaved += sizeSaved;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Total images optimized {files.Count()}, size reduced: {totalImageSizeSaved / 1024} kb");
+            Console.ResetColor();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(ex.Message);
+        Console.ResetColor();
+    }
+}, sourceArgument, outputOption, qualityOption);
+
+return rootCommand.Invoke(args);
